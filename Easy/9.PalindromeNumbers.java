f359d5af6ef8d718ecd2069742481ce545ef68ea
Brian Mendez <97647685+B1mendez@users.noreply.github.com>
2022-07-24 23:59:27 -0700

    Create 9.PalindromeNumbers.java
    Runtime: 8 ms, faster than 100.00% of Java online submissions for Palindrome Number. Memory Usage: 41.9 MB

---
+++ Easy/9.PalindromeNumbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if ((x != 0 && x % 10 == 0) || x < 0){
+            return false; 
+        }
+        
+        int reverseNum = 0;  
+        while (x > reverseNum){
+            reverseNum = reverseNum * 10 + x % 10; 
+            x = x /10; 
+        }
+        
+        return (reverseNum == x || x == reverseNum / 10); 
+    }
+}
